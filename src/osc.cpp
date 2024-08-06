0cf5f3dffba498a5ea8b01170362581063558588
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2024-08-06 13:45:08 +0900

    fix: add missing templates

---
+++ src/osc.cpp
@@ -0,0 +1,23 @@
+#include "osc.hpp"
+
+using namespace godot;
+
+
+// void GVVideoStream::_bind_methods()
+// {
+// 	ClassDB::bind_method(D_METHOD("get_on_memory"), &GVVideoStream::get_on_memory);
+//     ClassDB::bind_method(D_METHOD("set_on_memory", "value"), &GVVideoStream::set_on_memory);
+//     ClassDB::add_property(get_class_static(), PropertyInfo(Variant::BOOL, "on_memory"), "set_on_memory", "get_on_memory");
+
+//     ClassDB::bind_method(D_METHOD("get_pause_at_end"), &GVVideoStream::get_pause_at_end);
+//     ClassDB::bind_method(D_METHOD("set_pause_at_end", "value"), &GVVideoStream::set_pause_at_end);
+//     ClassDB::add_property(get_class_static(), PropertyInfo(Variant::BOOL, "pause_at_end"), "set_pause_at_end", "get_pause_at_end");
+// }
+
+OSC::OSC()
+{
+}
+
+OSC::~OSC()
+{
+}
--- src/osc.hpp
+++ src/osc.hpp
@@ -1,3 +1,14 @@
 #pragma once
 
+#include <godot_cpp/classes/node.hpp>
+#include <godot_cpp/core/class_db.hpp>
+
 using namespace godot;
+
+class OSC {
+
+public:
+    OSC();
+    ~OSC();
+
+};
